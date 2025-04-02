d15fc076ff45ea0b838c6ae810a8fa344999ef89
Joel Alberto Benítez Varela <joelalbertobenitezvarela28@gmail.com>
2025-04-01 23:49:41 -0400

    #configurando cadenas de conexion

---
+++ BreastIA/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TuProyecto.Data 
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        // Defimor los DbSet para cada tabla en la base de datos
+        public DbSet<Usuario> Usuarios { get; set; }
+        // Agrega más segun se vea necesario
+    }
+}
--- BreastIA/Program.cs
+++ BreastIA/Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using TuProyecto.Data;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configura la conexión a la base de datos
+var connectionString = builder.Configuration.GetConnectionString("Database_Connection_Services");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
+// Agrega servicios al contenedor.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configura Swagger/OpenAPI (opcional)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura la canalización de solicitudes HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,7 +26,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
